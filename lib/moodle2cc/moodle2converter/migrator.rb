f2de4a790a4169214932053b25fbdebdce11e5ab
Nathan Mills <nathanm@instructure.com>
2014-02-20 10:35:57 -0700

    add module conversion to converter

--- lib/moodle2cc/moodle2converter/migrator.rb
+++ lib/moodle2cc/moodle2converter/migrator.rb
@@ -8,6 +8,9 @@ class Moodle2CC::Moodle2Converter::Migrator
   def migrate
     moodle_course = @extractor.extract
     cc_course = Moodle2CC::Moodle2Converter::CourseConverter.new.convert(moodle_course)
+    cc_course.canvas_modules += moodle_course.sections.map do |section|
+      Moodle2CC::Moodle2Converter::SectionConverter.new.convert(section)
+    end
     @path = Moodle2CC::CanvasCC::CartridgeCreator.new(cc_course).create(@output_dir)
   end
 
--- test/unit/moodle2converter/migrator_test.rb
+++ test/unit/moodle2converter/migrator_test.rb
@@ -14,22 +14,47 @@ module Moodle2Converter
     end
 
     def test_migration
-      mock_extractor = MiniTest::Mock.new
-      mock_extractor.expect(:extract, nil)
-      mock_course_converter = MiniTest::Mock.new
-      mock_course_converter.expect(:convert, nil, [Object])
-      mock_cartridge_creator = MiniTest::Mock.new
-      mock_cartridge_creator.expect(:create, 'my_output_file', [Object])
-      Moodle2CC::Moodle2::Extractor.stub(:new, mock_extractor) do
-        Moodle2CC::Moodle2Converter::CourseConverter.stub(:new, mock_course_converter) do
-          Moodle2CC::CanvasCC::CartridgeCreator.stub(:new, mock_cartridge_creator) do
-            migrator = Moodle2CC::Moodle2Converter::Migrator.new('source_path', 'output_dir')
-            assert_equal(migrator.migrate, 'my_output_file')
+      canvas_course = Moodle2CC::CanvasCC::Model::Course.new
+      canvas_module = Moodle2CC::CanvasCC::Model::CanvasModule.new
+      extractor_mock do
+        mock_converter(Moodle2CC::Moodle2Converter::CourseConverter, canvas_course) do
+          mock_converter(Moodle2CC::Moodle2Converter::SectionConverter, canvas_module) do
+            cartridge_mock do
+              migrator = Moodle2CC::Moodle2Converter::Migrator.new('source_path', 'output_dir')
+              assert_equal(migrator.migrate, 'my_output_file')
+            end
           end
         end
       end
+    end
+
+    private
+
+    def mock_converter(converter_class, return_obj)
+      converter_mock = MiniTest::Mock.new
+      converter_mock.expect(:convert, return_obj, [Object])
+      converter_class.stub(:new, converter_mock) do
+        yield
+      end
+    end
+
+    def cartridge_mock
+      mock_cartridge_creator = MiniTest::Mock.new
+      mock_cartridge_creator.expect(:create, 'my_output_file', [Object])
+      Moodle2CC::CanvasCC::CartridgeCreator.stub(:new, mock_cartridge_creator) do
+        yield
+      end
+    end
+
+    def extractor_mock
+      moodle_course = Moodle2CC::Moodle2::Model::Course.new
+      moodle_course.sections << Moodle2CC::Moodle2::Model::Section.new
+      mock_extractor = MiniTest::Mock.new
+      mock_extractor.expect(:extract, moodle_course)
+      Moodle2CC::Moodle2::Extractor.stub(:new, mock_extractor) do
+        yield
+      end
       mock_extractor.verify
-      mock_course_converter.verify
     end
 
   end
