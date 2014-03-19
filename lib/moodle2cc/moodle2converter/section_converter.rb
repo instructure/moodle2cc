4c3a0e15ce445f163955a3b2b0005eaca96091f9
Brad Humphrey <brad@instructure.com>
2014-03-19 15:02:06 -0600

    add support for converting multiple types of activities

--- lib/moodle2cc/moodle2converter/section_converter.rb
+++ lib/moodle2cc/moodle2converter/section_converter.rb
@@ -2,31 +2,49 @@ module Moodle2CC
   class Moodle2Converter::SectionConverter
     include Moodle2Converter::ConverterHelper
 
+    ACTIVITY_CONVERTERS = {
+        Moodle2::Models::Book => Moodle2Converter::BookConverter
+    }
+
+    def initialize
+      @converters ={}
+    end
 
     def convert(moodle_section)
       canvas_module = CanvasCC::Models::CanvasModule.new
       canvas_module.identifier = generate_unique_identifier_for(moodle_section.id, MODULE_SUFFIX)
       canvas_module.title = moodle_section.name
       canvas_module.workflow_state = moodle_section.visible ? CanvasCC::Models::WorkflowState::ACTIVE : CanvasCC::Models::WorkflowState::UNPUBLISHED
-      canvas_module.module_items += moodle_section.activities.map { |a| convert_activity(a) }.compact
+      canvas_module.module_items += moodle_section.activities.map { |a| convert_activity(a) }.flatten.compact
 
       canvas_module
     end
 
     def convert_activity(moodle_activity)
       begin
-        module_item = CanvasCC::Models::ModuleItem.new
-        module_item.identifier = generate_unique_identifier
-        module_item.workflow_state = CanvasCC::Models::WorkflowState::ACTIVE
-        module_item.title = moodle_activity.name
-        module_item.identifierref = generate_unique_identifier_for_activity(moodle_activity)
-        module_item.content_type = activity_content_type(moodle_activity)
-        module_item.indent = '0'
-
-        module_item
+        activity_converter_for(moodle_activity).convert_to_module_items(moodle_activity)
       rescue Exception => e
         puts e.message
       end
+    end
+
+    def convert_to_module_items(moodle_activity)
+      module_item = CanvasCC::Models::ModuleItem.new
+      module_item.identifier = generate_unique_identifier
+      module_item.workflow_state = CanvasCC::Models::WorkflowState::ACTIVE
+      module_item.title = moodle_activity.name
+      module_item.identifierref = generate_unique_identifier_for_activity(moodle_activity)
+      module_item.content_type = activity_content_type(moodle_activity)
+      module_item.indent = '0'
+
+      [module_item]
+    end
+
+    private
+
+    def activity_converter_for(moodle_activity)
+      @converters[moodle_activity.class] ||=
+        ACTIVITY_CONVERTERS[moodle_activity.class] ? ACTIVITY_CONVERTERS[moodle_activity.class].new : self
     end
 
   end
--- spec/moodle2cc/moodle2converter/section_converter_spec.rb
+++ spec/moodle2cc/moodle2converter/section_converter_spec.rb
@@ -3,39 +3,65 @@ require 'spec_helper'
 module Moodle2CC
   describe Moodle2Converter::SectionConverter do
 
-    it 'should convert a moodle section to a canvas module' do
-      moodle_section = Moodle2::Model::Section.new
-      moodle_section.id = 'section_id'
-      moodle_section.name = 'section_name'
-      moodle_section.visible = false
-      moodle_section.position = 1
-      canvas_module = subject.convert(moodle_section)
-      expect(canvas_module.identifier).to eq('m2730f6511535a1e4cf13e886e52b21dc9_module')
-      expect(canvas_module.title).to eq('section_name')
-      expect(canvas_module.workflow_state).to eq('unpublished')
+    describe '#convert' do
+      let(:moodle_section) { Moodle2::Models::Section.new }
+
+      it 'should convert a moodle section to a canvas module' do
+        moodle_section.id = 'section_id'
+        moodle_section.name = 'section_name'
+        moodle_section.visible = false
+        moodle_section.position = 1
+        canvas_module = subject.convert(moodle_section)
+        expect(canvas_module.identifier).to eq('m2730f6511535a1e4cf13e886e52b21dc9_module')
+        expect(canvas_module.title).to eq('section_name')
+        expect(canvas_module.workflow_state).to eq('unpublished')
+      end
+
+      it 'converts all activities to module_items' do
+        subject.stub(:convert_activity) { [:module_item] }
+        3.times { moodle_section.activities << [:activity] }
+        canvas_module = subject.convert(moodle_section)
+
+        expect(canvas_module.module_items).to eq [:module_item, :module_item, :module_item]
+      end
     end
 
-    it 'converts a moodle page activity to a canvas module item' do
-      subject.stub(:generate_unique_identifier) {'some_random_id'}
+    describe '#convert_activity' do
+      it 'uses the default converter for pages' do
+        subject.stub(:convert_to_module_items) { [:module_item] }
+        module_items = subject.convert_activity(Moodle2::Models::Page.new)
 
-      moodle_section = Moodle2::Models::Section.new
-      moodle_page = Moodle2::Models::Page.new
-      moodle_page.id = '1'
-      moodle_page.name = 'page title'
+        expect(module_items).to eq [:module_item]
+      end
 
-      moodle_section.sequence << moodle_page.id
-      moodle_section.activities << moodle_page
+      it 'uses the book converter for books' do
+        Moodle2Converter::BookConverter.any_instance.stub(:convert_to_module_items) { [:module_item] }
+        module_items = subject.convert_activity(Moodle2::Models::Book.new)
 
-      canvas_module = subject.convert(moodle_section)
+        expect(module_items).to eq [:module_item]
+      end
+    end
 
-      expect(canvas_module.module_items.size).to eq 1
-      module_item = canvas_module.module_items.first
-      expect(module_item.identifier).to eq 'some_random_id'
-      expect(module_item.workflow_state).to eq 'active'
-      expect(module_item.title).to eq 'page title'
-      expect(module_item.identifierref).to eq 'm2c4ca4238a0b923820dcc509a6f75849b_page'
-      expect(module_item.content_type).to eq 'WikiPage'
-      expect(module_item.indent).to eq '0'
+    describe '#convert_to_module_items' do
+      it 'converts a moodle page to a module item' do
+        subject.stub(:generate_unique_identifier) { 'some_random_id' }
+
+        moodle_page = Moodle2::Models::Page.new
+        moodle_page.id = '1'
+        moodle_page.name = 'page title'
+
+        module_items = subject.convert_to_module_items(moodle_page)
+        expect(module_items.size).to eq 1
+
+        module_item = module_items.first
+
+        expect(module_item.identifier).to eq 'some_random_id'
+        expect(module_item.workflow_state).to eq 'active'
+        expect(module_item.title).to eq 'page title'
+        expect(module_item.identifierref).to eq 'm2c4ca4238a0b923820dcc509a6f75849b_page'
+        expect(module_item.content_type).to eq 'WikiPage'
+        expect(module_item.indent).to eq '0'
+      end
     end
   end
 end
