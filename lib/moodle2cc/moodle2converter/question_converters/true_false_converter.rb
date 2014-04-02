f267e9f845cf9f0fc4d9e98945bccf562c9a5d34
James Williams <jamesw@instructure.com>
2014-04-02 14:14:16 -0600

    fix true false questions

--- lib/moodle2cc/moodle2converter/question_converters/true_false_converter.rb
+++ lib/moodle2cc/moodle2converter/question_converters/true_false_converter.rb
@@ -7,10 +7,7 @@ module Moodle2CC::Moodle2Converter
       def convert_question(moodle_question)
         moodle_question.answers = moodle_question.answers.select{|a|
           [moodle_question.true_answer.to_s, moodle_question.false_answer.to_s].include?(a.id.to_s)
-        }.map do |a|
-          a.fraction = (moodle_question.true_answer.to_s == a.id.to_s) ? 1.0 : 0.0
-          a
-        end
+        }
 
         canvas_question = super(moodle_question)
         canvas_question
