36d5572b0aad8edb4f14e09299a253dff531b87e
James Williams <jamesw@instructure.com>
2015-09-23 08:31:47 -0600

    fix specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,12 @@ require 'rspec'
 require 'moodle2cc'
 Dir["./spec/support/**/*.rb"].sort.each {|f| require f}
 
+RSpec.configure do |c|
+  c.after :each do
+    Moodle2CC::Moodle2Converter::ConverterHelper.class_variable_set(:@@ids, nil)
+  end
+end
+
 def fixture_path(path)
   File.join(File.expand_path("../../test/fixtures", __FILE__), path)
 end
